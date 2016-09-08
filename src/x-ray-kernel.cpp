5ad800c4673243f02565660987645fab76cbe698
Nidin Vinayakan <01@01alchemist.com>
2016-09-08 16:56:06 +0200

    Adding simple siles

---
+++ src/x-ray-kernel.cpp
@@ -0,0 +1,14 @@
+/**
+ * ...
+ * @author Nidin Vinayakan
+ * @company 01 Alchemist | Munich | Germany
+ */
+#include <stdio.h>
+#include <string.h>
+#include <sstream>
+
+extern "C" {
+    int newScene(int scene){
+        return scene;
+    }
+}
