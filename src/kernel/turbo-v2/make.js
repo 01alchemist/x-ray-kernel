3a1e397eaae7d56cd9b11821c30f70dbd9bc7bc7
Nidin Vinayakan <nidinthb@gmail.com>
2017-04-30 10:01:22 +0200

    sign off

--- src/kernel/turbo-v2/make.js
+++ src/kernel/turbo-v2/make.js
@@ -4,7 +4,7 @@ let shell = require("shelljs");
 let fs = require("fs");
 let path = require("path");
 // let modules = [ //     "./src/turbo/common.tts", //     "./src/tracer/axis.tts", //     "./src/turbo/color.tts", //     "./src/turbo/vector.tts", //     "./src/utils/util.tts", //     "./src/turbo/box.tts", //     "./src/turbo/matrix.tts", //     "./src/turbo/image.tts", //     "./src/turbo/texture.tts", //     "./src/turbo/material.tts", //     "./src/tracer/ray.tts", // //     "./src/turbo/shapes/shape.tts", //     "./src/turbo/shapes/cube.tts", //     "./src/turbo/shapes/sphere.tts", //     "./src/turbo/shapes/triangle.tts", //     "./src/turbo/shapes/mesh.tts", // //     "./src/turbo/tree.tts", //     "./src/tracer/hit.tts", //     "./src/turbo/camera.tts", //     "./src/turbo/scene.tts", //     "./src/three/buffer_geometry.tts", //     "./src/tracer/sampler.tts", // //     "./src/tracer/vector3.tts", //     "./src/tracer/color3.tts", //     "./src/tracer/matrix4.tts", // // ];
-let modules = ["./src/turbo/vector.tbs"];
+let modules = ["./src/turbo/vector3.tbs"];
 let buildCommand = [];
 modules.forEach((file) => {
     buildCommand.push(path.resolve(__dirname, file));
@@ -13,7 +13,7 @@ modules.forEach((file) => {
 let TURBO_PATH = path.resolve(__dirname, "../../../../TurboScript/");
 process.env.TURBO_PATH = TURBO_PATH;
 
-let outFile = path.resolve(__dirname, "xray-kernel-turbo.asm.js");
+let outFile = path.resolve(__dirname, "xray-kernel-turbo");
 
 buildCommand.push("--asmjs", "--out", outFile);
 
---
+++ src/kernel/turbo-v2/test/asm.js/main.js
@@ -0,0 +1,3 @@
+/**
+ * Created by nidin on 2017-02-03.
+ */
